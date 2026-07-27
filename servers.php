bce1fdb01dc46491f845dce53e4ed650dc5cec4a
Felix Scholzen <89754142+felixs274@users.noreply.github.com>
2026-07-27 18:02:39 +0200

    Create servers.php

---
+++ servers.php
@@ -0,0 +1,12 @@
+[
+  {
+    "name": "Glasfaser Direkt",
+    "server": "https://speedtest.glasfaser-direkt.de",
+    "dlURL": "/backend/garbage.php",
+    "ulURL": "/backend/empty.php",
+    "pingURL": "/backend/empty.php",
+    "getIpURL": "/backend/getIP.php",
+    "sponsorName": "Glasfaser Direkt",
+    "sponsorURL": "https://glasfaser-direkt.de/"
+  }
+]
